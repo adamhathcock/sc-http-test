85780a5e7eaa9a73eeb8f4730caa2dcc78d06cb4
Adam Hathcock <adam@hathcock.uk>
2017-06-04 07:02:07 +0100

    no exceptions here

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SharpCompress.Readers;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test().Wait();
+        }
+
+        public static async Task Test()
+        {
+            using(var client = new HttpClient())
+            using (var response = await client.GetAsync("https://github.com/adamhathcock/sharpcompress/raw/master/tests/TestArchives/Archives/Tar.tar",
+                    HttpCompletionOption.ResponseHeadersRead))
+            {
+                using(var stream = await response.Content.ReadAsStreamAsync())
+                using (var reader = ReaderFactory.Open(stream))
+                {
+                    reader.MoveToNextEntry();
+                    reader.WriteEntryTo(new MemoryStream());  //System
+                }
+            }
+        }
+    }
+}
